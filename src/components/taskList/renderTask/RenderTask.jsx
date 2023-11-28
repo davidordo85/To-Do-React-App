a3c979933d41930a555ddcfcc760d22789a85a3a
David Ordoñez <davidordonezescacena@hotmail.es>
2023-11-28 21:03:14 +0100

    add draggable for moving task

--- src/components/taskList/renderTask/RenderTask.jsx
+++ src/components/taskList/renderTask/RenderTask.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Draggable from 'react-draggable';
 import Alert from '../../shared/alert/Alert';
 import Buttons from '../../shared/buttons';
 import './RenderTask.css';
@@ -36,99 +37,105 @@ const RenderTask = ({
     console.log('borrar task', index, updateData);
   };
 
-  console.log(color);
+  const taskRef = React.useRef(null);
 
   return (
-    <div className="task-container" style={{ backgroundColor: color }}>
-      {showConfirmAlert ? (
-        <Alert
-          message={'Are you sure you want to delete this task?'}
-          onClose={() => setShowConfirmAlert(false)}
-          onConfirm={() => handleDeleteTask(index)}
-        />
-      ) : null}
-      <div className="options-task-container">
-        <Buttons
-          label="..."
-          ariaLabel="options"
-          className="option-task-button"
-          type="button"
-          onClick={handleToggleExpand}
-        />
-        {optionExpanded && (
-          <div className="options-task-dropdown">
-            <Buttons
-              label="Detail"
-              className="detail-task-button"
-              ariaLabel="Detail task"
-              type="button"
-              onClick={handleDetailExpand}
-            />
-            <Buttons
-              label="Modify"
-              className="modify-task-button"
-              ariaLabel="Modify task"
-              type="button"
-              /* onClick={handleDeleteTask} */
-            />
-            <Buttons
-              label="Delete"
-              className="delete-task-button"
-              ariaLabel="delete task"
-              type="button"
-              onClick={handleDeleteTask}
-            />
-          </div>
-        )}
-      </div>
-      <div className="data-container">
-        <div>
-          <p className="task-paragraph">
-            <strong>Task:</strong>
-          </p>
-          <span className="task-contain">{text}</span>
-        </div>
-      </div>
-      {isExpanded ? (
-        <>
-          {' '}
-          <div className="data-container">
-            <p className="task-paragraph">
-              <strong>Due date:</strong>
-            </p>
-            <span className="task-contain">{formatDate(date)}</span>
-          </div>
-          <div className="data-container">
-            <p className="task-paragraph">
-              <strong>Description:</strong>
-            </p>
-            <span className="task-contain">{description}</span>
-          </div>
-          {estimatedDuration > 0 && (
-            <div className="data-container">
-              <p className="task-paragraph">
-                <strong>Estimated duration:</strong>
-              </p>
-              <span className="task-contain">
-                {estimatedDuration} {estimatedDuration > 1 ? 'hours' : 'hour'}
-              </span>
+    <Draggable nodeRef={taskRef}>
+      <div
+        className="task-container"
+        style={{ backgroundColor: color }}
+        ref={taskRef}
+      >
+        {showConfirmAlert ? (
+          <Alert
+            message={'Are you sure you want to delete this task?'}
+            onClose={() => setShowConfirmAlert(false)}
+            onConfirm={() => handleDeleteTask(index)}
+          />
+        ) : null}
+        <div className="options-task-container">
+          <Buttons
+            label="..."
+            ariaLabel="options"
+            className="option-task-button"
+            type="button"
+            onClick={handleToggleExpand}
+          />
+          {optionExpanded && (
+            <div className="options-task-dropdown">
+              <Buttons
+                label="Detail"
+                className="detail-task-button"
+                ariaLabel="Detail task"
+                type="button"
+                onClick={handleDetailExpand}
+              />
+              <Buttons
+                label="Modify"
+                className="modify-task-button"
+                ariaLabel="Modify task"
+                type="button"
+                /* onClick={handleDeleteTask} */
+              />
+              <Buttons
+                label="Delete"
+                className="delete-task-button"
+                ariaLabel="delete task"
+                type="button"
+                onClick={handleDeleteTask}
+              />
             </div>
           )}
-          <div className="data-container">
+        </div>
+        <div className="data-container">
+          <div>
             <p className="task-paragraph">
-              <strong>Created date:</strong>
+              <strong>Task:</strong>
             </p>
-            <span className="task-contain">{formatDate(createdDate)}</span>
+            <span className="task-contain">{text}</span>
           </div>
-          <div className="data-container">
-            <p className="task-paragraph">
-              <strong>Priority:</strong>
-            </p>
-            <span className="task-contain">{priority}</span>
-          </div>
-        </>
-      ) : null}
-    </div>
+        </div>
+        {isExpanded ? (
+          <>
+            {' '}
+            <div className="data-container">
+              <p className="task-paragraph">
+                <strong>Due date:</strong>
+              </p>
+              <span className="task-contain">{formatDate(date)}</span>
+            </div>
+            <div className="data-container">
+              <p className="task-paragraph">
+                <strong>Description:</strong>
+              </p>
+              <span className="task-contain">{description}</span>
+            </div>
+            {estimatedDuration > 0 && (
+              <div className="data-container">
+                <p className="task-paragraph">
+                  <strong>Estimated duration:</strong>
+                </p>
+                <span className="task-contain">
+                  {estimatedDuration} {estimatedDuration > 1 ? 'hours' : 'hour'}
+                </span>
+              </div>
+            )}
+            <div className="data-container">
+              <p className="task-paragraph">
+                <strong>Created date:</strong>
+              </p>
+              <span className="task-contain">{formatDate(createdDate)}</span>
+            </div>
+            <div className="data-container">
+              <p className="task-paragraph">
+                <strong>Priority:</strong>
+              </p>
+              <span className="task-contain">{priority}</span>
+            </div>
+          </>
+        ) : null}
+      </div>
+    </Draggable>
   );
 };
 
